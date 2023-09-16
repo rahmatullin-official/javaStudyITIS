2ba8b9941b9851a6449d1f2302cd5b4c9ac0091b
Damir Rakhmatullin <75735399+rahmatullin-official@users.noreply.github.com>
2023-09-16 14:11:52 +0300

    task 3 done

--- Test.java
+++ Test.java
@@ -4,14 +4,16 @@ import java.lang.Math;
 public class Test {
     public static void main(String[] args) {
        int [] arr = new int[100];
-       int N = 2;
+       int start = 1;
+       int end = 100;
        for(int i = 0; i < 100; i++){
            if (i % 2 == 0){
-                arr[i] = N;
+                arr[i] = start;
+                start++;
            }
            else{
-               arr[i] = N - 1;
-               N += 2;
+                arr[i] = end;
+                end--;
            }
        }
         for(int i = 0; i < 100; i++){
