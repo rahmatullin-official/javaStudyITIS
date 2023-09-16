9570b865b5d9d2b0abb2c028bba8b637d0e8c8ce
Damir Rakhmatullin <75735399+rahmatullin-official@users.noreply.github.com>
2023-09-16 13:14:36 +0300

    second task done@

--- Test.java
+++ Test.java
@@ -3,21 +3,14 @@ import java.lang.Math;
 
 public class Test {
     public static void main(String[] args) {
-        for (int i = 1000; i < 10000; i++){
-
-            int fourthDigit = i % 10;
-            int thirdDigit = i % 100 / 10;
-            int secondDigit = i % 1000 / 100;
-            int firstDigit = i / 1000;
-
-            if (i % 2 == 0 && i % 7 == 0 && i % 11 == 0){
-                if ((fourthDigit + thirdDigit + secondDigit + firstDigit) == 30){
-                    if ((firstDigit == secondDigit && thirdDigit == fourthDigit)
-                            || (firstDigit == thirdDigit && secondDigit == fourthDigit)
-                            || (firstDigit ==fourthDigit && secondDigit == thirdDigit)) {
-                        System.out.println(i);
-                    }
-                }
+        Scanner scan = new Scanner(System.in);
+        int N = scan.nextInt();
+        for (int i = 100; i < 1000; i++){
+            int firstDigit = i / 100;
+            int secondDigit = i % 100 / 10;
+            int thirdDigit = i % 10;
+            if ((firstDigit + secondDigit + thirdDigit) == N){
+                System.out.println(i);
             }
         }
     }
