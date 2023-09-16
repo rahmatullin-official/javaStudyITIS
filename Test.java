dab7201e885686ae527abec1bdaaf5c92da99a76
Damir Rakhmatullin <75735399+rahmatullin-official@users.noreply.github.com>
2023-09-16 14:45:58 +0300

    task 4 done

--- Test.java
+++ Test.java
@@ -1,24 +1,22 @@
+import java.util.Random;
 import java.util.Scanner;
 import java.lang.Math;
 
 public class Test {
     public static void main(String[] args) {
-       int [] arr = new int[100];
-       int start = 1;
-       int end = 100;
-       for(int i = 0; i < 100; i++){
-           if (i % 2 == 0){
-                arr[i] = start;
-                start++;
-           }
-           else{
-                arr[i] = end;
-                end--;
+       int [] arr = new int[75];
+       int [] y = new int[75];
+       int counter = 0;
+       for (int i = 0; i < 75; i++){
+           double rand = Math.random() * 45 - 5;
+           arr[i] = (int)rand;
+           if (arr[i] < 20) {
+               y[counter] = arr[i];
+               counter += 1;
            }
        }
-        for(int i = 0; i < 100; i++){
-            System.out.print(arr[i] + " ");
-        }
-
+       for(int i = 0; i < 75; i++){
+           System.out.print(y[i] + " ");
+       }
     }
 }
